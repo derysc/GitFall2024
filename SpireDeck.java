fd5138c9800dc133df5ffa983295fe109ad5adc1
Derys Coreas <derys2004@gmail.com>
2024-09-23 22:09:00 -0500

    Added filereading method to SpireDeck.java & added to .gitignore
    Got filereading to work in SpireDeck.java. Currently reads a file and prints the line.
    Added *.class to .gitignore ass they aren't useful for the repository

--- SpireDeck.java
+++ SpireDeck.java
@@ -1,2 +1,24 @@
-public class SpireDeck {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class SpireDeck
+{
+
+    public static Scanner scan;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please type in the correctly formated file name with your deck");
+        String filename = input.next();
+        costTally(filename);
+
+    }
+
+    public static void costTally(String filename) throws FileNotFoundException {
+        scan = new Scanner(new File(filename));
+        System.out.println(scan.next());
+
+
+    }
 }
