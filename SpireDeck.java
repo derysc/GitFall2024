b133508f186a9013b334e540dcdf0e5305f36f2e
Derys Coreas <derys2004@gmail.com>
2024-09-23 22:46:12 -0500

    Added costTally Method to SpireDeck.java
    Added a costTally method that reads the next file string and checks to see if the energy cost amount is valid.

--- SpireDeck.java
+++ SpireDeck.java
@@ -6,19 +6,47 @@ public class SpireDeck
 {
 
     public static Scanner scan;
+    public static int deckId;
+    public static int totalCost = 0;
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner input = new Scanner(System.in);
         System.out.println("Please type in the correctly formated file name with your deck");
         String filename = input.next();
-        costTally(filename);
+        fileReader(filename);
 
     }
 
-    public static void costTally(String filename) throws FileNotFoundException {
+    public static void fileReader(String filename) throws FileNotFoundException {
         scan = new Scanner(new File(filename));
-        System.out.println(scan.next());
+        Random random = new Random();
+        if (scan.hasNext() == true){
+            deckId = random.nextInt(111111111,999999999);
+            System.out.println(deckId);
+        } else {
+            System.out.println("This file is empty");
+            return;
+        }
+        while (scan.hasNext()) {
+            costTally(scan.next());
+        }
 
+
+
+    }
+
+    public static void costTally(String cardInfo){
+        String [] temp = cardInfo.split(":");
+        if (temp.length == 1) {
+            return;
+        }
+        int cost = Integer.parseInt(temp[1]);
+        if ((cost >= 0) && (cost <= 6)) {
+            totalCost += cost;
+        } else {
+            System.out.println(cardInfo + " has an invalid engery cost amount");
+        }
+        System.out.println(totalCost);
 
     }
 }
